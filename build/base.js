8eaef53ad70477d0c8f25de3c41b6c3aafa5a8f8
horseslee <>
2019-12-10 13:51:56 +0800

    rollup bundle done, begin to use webpack

--- build/base.js
+++ build/base.js
@@ -3,6 +3,19 @@ import typescriptPlugin from "rollup-plugin-typescript";
 import aliasPlugin from "@rollup/plugin-alias";
 import scssPlugin from "rollup-plugin-scss";
 import byyPlugin from "./plugins/rollup-plugin-moon";
+import babel from "rollup-plugin-babel";
+import babelrc from "babelrc-rollup";
+
+const babelConfig = {
+  'presets': [
+    ['env', {
+      'targets': {
+        'browsers': ['IE 8']
+      },
+      'loose': true
+    }]
+  ]
+};
 
 const root = path.resolve(__dirname, "../");
 
@@ -14,21 +27,26 @@ base = {
   output: (name) => {
     return {
       file: `dist/js/${name}.js`,
-      format: "es"
+      format: "cjs"
     }
   },
 
   plugins: (name) => {
     return [
-      byyPlugin({
-        name: "123"
-      }),
+      babel(babelrc({
+        addExternalHelpersPlugin: true,
+        config: babelConfig,
+        exclude: 'node_modules/**'
+      })),
       aliasPlugin({
         resolve: ['.js', '.ts', '.moon', '.scss'],
         entries: {
           "__scss": path.resolve(urls.srcRoot, "scss"),
           "__components": path.resolve(urls.srcRoot, "components")
         }
+      }),
+      byyPlugin({
+        name: "123"
       }),
       scssPlugin({
         output: `dist/css/${name}.css`
--- build/plugins/rollup-plugin-moon/index.js
+++ build/plugins/rollup-plugin-moon/index.js
@@ -1,5 +1,6 @@
 import { SplitCode } from "./splitCode";
 import { format } from "esformatter";
+import * as ts from "typescript";
 import TransformRender from "./trasformToRender";
 
 export default function byy (options) {
@@ -12,6 +13,21 @@ export default function byy (options) {
       if (!regExp.test(id)) return null;
       
       let { template, script} = new SplitCode(code);
+
+      // let test = ts.transpileModule(script, {
+      //   compilerOptions: {
+      //     module: ts.ModuleKind.UMD,
+      //     target: ts.ScriptTarget.ES5,
+      //     esModuleInterop: false,
+      //     strict: false,
+      //     lib: [
+      //       ts.ScriptTarget.ES5,
+      //       ts.ScriptTarget
+      //     ]
+      //   }
+      // });
+
+      // console.log("sss", test.outputText);
 
       let renderTransformer = new TransformRender(template);
       
