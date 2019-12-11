7c66c301a1930c28d0b82b29b2fca6b107505533
Horses.Lee <1142719994@qq.com>
2019-12-11 21:18:49 +0800

    delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-var TestOne = /** @class */ (function () {
-    function TestOne(name) {
-        this.name = name;
-    }
-    TestOne.prototype.__init = function () {
-        console.log(11111, this.name);
-    };
-    return TestOne;
-}());
